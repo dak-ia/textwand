3eb002362759dec854a7ba56fc75ef48570696a9
dak-ia <97687749+dak-ia@users.noreply.github.com>
2026-07-05 01:33:37 +0900

    refactor: constantsバレルをexport *形式に統一

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export { FULLWIDTH_KANA_KEYMAP } from "./fullwidthKanaKeymap";
-export { HALFWIDTH_KANA_KEYMAP } from "./halfwidthKanaKeymap";
-export { HIRAGANA_KEYMAP } from "./hiraganaKeymap";
+export * from "./fullwidthKanaKeymap";
+export * from "./halfwidthKanaKeymap";
+export * from "./hiraganaKeymap";
