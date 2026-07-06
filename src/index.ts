31d19811401559ef9b9801d8bf506af36e5ac8ad
dak-ia <97687749+dak-ia@users.noreply.github.com>
2026-07-07 06:04:46 +0900

    feat: アプリ全体のHTMLとエントリ配線・copyright import統一を追加 (#29)
    * feat: アプリ全体のHTMLとレイアウトCSSとエントリ配線を追加
    
    * refactor: copyrightのimportをutilsバレル経由に統一

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import "./styles/index.css";
+import "./ui/convert";
 import "./ui/copyright";
+import "./ui/count";
+import "./ui/editor";
+import "./ui/highlight";
+import "./ui/tabs";
+import "./ui/theme";
